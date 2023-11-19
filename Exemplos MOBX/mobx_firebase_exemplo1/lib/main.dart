9b9c56a1be46f3b28630c9eaf6933225d79fcacc
Gabriel Nascimento <gabriel_maker@hotmail.com>
2023-11-19 12:03:36 -0300

    news

--- Exemplos MOBX/mobx_firebase_exemplo1/lib/main.dart
+++ Exemplos MOBX/mobx_firebase_exemplo1/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mobx_firebase_exemplo1/firebase_options.dart';
 import 'package:mobx_firebase_exemplo1/screens/login/hello.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 const bool USE_EMULATOR = true;
@@ -31,7 +31,7 @@ Future _connectToFirebaseEmulator() async {
       persistenceEnabled: false);
 
   await FirebaseAuth.instance.useAuthEmulator(localHost, authPort);
-  await FirebaseStorage.instance.useStorageEmulator(localHost, storagePort);
+  //await FirebaseStorage.instance.useStorageEmulator(localHost, storagePort);
 }
 
 class MyApp extends StatelessWidget {
